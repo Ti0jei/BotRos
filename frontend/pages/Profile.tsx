d01585aa709e102b377115e7c3e8ea838b19bccc
Ti0jei <zoty2104@gmail.com>
2025-07-01 16:43:30 +0300

    ✨ делаю профиль

--- frontend/pages/Profile.tsx
+++ frontend/pages/Profile.tsx
@@ -146,7 +146,7 @@ export default function Profile({
           title={
             <div className="flex items-center justify-between w-full">
               <span className="text-lg font-bold text-black">
-                Привет, {user.name} 👋
+                Привет, {user.lastName} 👋
               </span>
               <Tooltip
                 label={
