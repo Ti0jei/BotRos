0be890a49d5d2cd29df1dae646bab2b646921762
Ti0jei <zoty2104@gmail.com>
2025-07-07 01:51:30 +0300

    fix: полный контроль выбора времени в AssignModal + проверка компонентов (v0.2.8)

--- frontend/pages/AssignTrainingPage.tsx
+++ frontend/pages/AssignTrainingPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import AssignModal from "./AssignModal";
+import AssignModal from "./AdminSchedule/AssignModal";
 import { User, PaymentBlock } from "./types";
 
 export default function AssignTrainingPage() {
