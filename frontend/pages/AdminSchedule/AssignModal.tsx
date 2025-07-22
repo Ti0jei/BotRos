e3ca9a764f10c6ce3adbe3e80826e252957d612c
Ti0jei <zoty2104@gmail.com>
2025-07-22 23:36:49 +0300

    Fix: белый экран и зависание клавиатуры  добавлен TextInput и безопасный parseInt(singlePrice)

--- frontend/pages/AdminSchedule/AssignModal.tsx
+++ frontend/pages/AdminSchedule/AssignModal.tsx
@@ -16,7 +16,7 @@ import {
   NumberInput,
   Box,
   ScrollArea,
-  TextInput 
+  TextInput,
 } from "@mantine/core";
 import { IconClock, IconX } from "@tabler/icons-react";
 import dayjs, { Dayjs } from "dayjs";
