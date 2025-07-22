bde1e50fb0072909cd48d842be40a08fad2d43c8
Ti0jei <zoty2104@gmail.com>
2025-07-23 00:04:55 +0300

    Fix: белый экран и зависание клавиатуры  добавлен TextInput и безопасный parseInt(singlePrice)

--- frontend/pages/AdminSchedule/AssignModal.tsx
+++ frontend/pages/AdminSchedule/AssignModal.tsx
@@ -287,8 +287,8 @@ export default function AssignModal({
                   }
                 }}
                 min={0}
+                radius="xl"
                 hideControls
-                radius="md"
                 onBlur={blurActiveElement}
               />
 
